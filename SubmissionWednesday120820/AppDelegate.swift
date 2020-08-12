88dc841bae45430da165e062f3b002005a8ab5ba
Tushar <tushare0509@gmail.com>
2020-08-12 18:35:35 +0530

    No Output

--- SubmissionWednesday120820/AppDelegate.swift
+++ SubmissionWednesday120820/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        let window = UIWindow()
+//        let layout = UICollectionViewFlowLayout()
+//        window.makeKeyAndVisible()
+//        window.rootViewController = ViewController(collectionViewLayout: layout)
         // Override point for customization after application launch.
         return true
     }
--- SubmissionWednesday120820/ViewController.swift
+++ SubmissionWednesday120820/ViewController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let instaid = "instagramCellId"
+    let youtid = "youtubeCellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Entering Function")
+        setupviewcontroller()
+      //  collectionView.backgroundColor = .red
+        print("Exiting")
         // Do any additional setup after loading the view.
     }
 
+    fileprivate func setupviewcontroller(){
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(InstagramCollectionViewCell.self, forCellWithReuseIdentifier: instaid)
+        collectionView.register(YoutubeCell.self, forCellWithReuseIdentifier: youtid)
+        collectionView.contentInset = UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0)
+        
+    }
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
 
+     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+       
+        switch indexPath.item {
+        case 0:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: instaid, for: indexPath) as! InstagramCollectionViewCell
+                   return cell
+        case 1:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: instaid, for: indexPath) as! InstagramCollectionViewCell
+            return cell
+        default:
+            return UICollectionViewCell()
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        switch indexPath.item {
+        case 0:
+            return CGSize(width: view.frame.width, height: 300)
+        case 1:
+            return CGSize(width: view.frame.width, height: 320)
+        default:
+            return CGSize(width: view.frame.width, height: 200)
+        }
+    }
 }
 
